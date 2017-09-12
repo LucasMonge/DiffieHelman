dd7e1e27b8e32c8b0bd9806b282163e347f8dfb4
unknown <antobrom100@W7D-CQW9G52.student.kau.se>
2017-09-12 10:18:45 +0200

    client first test

--- client.c
+++ client.c
@@ -1,3 +1,35 @@
-//Test 2 3
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
 
-//TEst 2
+int main(){
+  int clientSocket;
+  char buffer[1024];
+  struct sockaddr_in serverAddr;
+  socklen_t addr_size;
+  int port = 12345;
+
+  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+  
+  serverAddr.sin_family = AF_INET;
+  
+  serverAddr.sin_port = htons(port);
+  
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+
+
+  addr_size = sizeof serverAddr;
+  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+
+  //Receved message
+  recv(clientSocket, buffer, 1024, 0);
+
+	//message is printed
+  printf("Data received: %s",buffer);   
+
+  return 0;
+}
