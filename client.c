4ff92d7ea63d831c2f935b7080cd0e238e2d05b0
LucasMonge <lucas.monge95@gmail.com>
2017-09-18 17:35:41 +0200

    Changes: - Just some fix

--- client.c
+++ client.c
@@ -8,7 +8,7 @@
 #include <unistd.h>
 
 #define PORT 12345
-#define  MESSAGELEN  4
+#define  MESSAGELEN  30
 #define  CIPHERTEXT_LEN ( crypto_secretbox_MACBYTES +MESSAGELEN )
 
 
@@ -20,50 +20,49 @@ int main(){
 	unsigned char buffer[1024];
 	struct sockaddr_in serverAddr;
 	socklen_t addr_size;
-	unsigned char message[4] = "Test";
+	unsigned char message[MESSAGELEN] = "Hello world";
 		
-		//Creation of the socket (see server code)
-		clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+	//Creation of the socket (see server code)
+	clientSocket = socket(PF_INET, SOCK_STREAM, 0);
 
-		//Parameters of the server
-		serverAddr.sin_family = AF_INET;
-		serverAddr.sin_port = htons(PORT);
-		serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	//Parameters of the server
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-		//Connect to the server
-		addr_size = sizeof(serverAddr);
-		connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
-				//Received message
-				
-		recv(clientSocket, buffer, 1024, 0);
+	//Connect to the server
+	addr_size = sizeof(serverAddr);
+	connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+	
+	//Received message	
+	recv(clientSocket, buffer, 1024, 0);
 
-		//message is printed
-		printf("Data received: %s",buffer); 
+	//message is printed
+	printf("Data received: %s",buffer); 
 
 	while(1){
 	
-		printf("Choices:\n 1: send message\n  2:end communication\n");
+		printf("\nChoices:\n 1: send message\n 2: end communication\n");
 		scanf("%d", &choice);
 		switch(choice){
 			case 1:
-				crypto_secretbox_easy(ciphertext, message, sizeof(message), nonce, key);
 				//Send a message
-				//printf("Test");
+				crypto_secretbox_easy(ciphertext, message, sizeof(message), nonce, key);
 				if(sendto(clientSocket, ciphertext, sizeof(ciphertext), 0, (struct sockaddr *)&serverAddr, addr_size)<0)
 					perror("ERROR message not send");
 				break;
 			case 2:
+				//Send a message to warn the server
 				if(sendto(clientSocket, "Exit", sizeof("Exit"), 0, (struct sockaddr *)&serverAddr, addr_size)<0)
 					perror("ERROR message not send");
+				//Close the socket
 				close(clientSocket);
 				return 0;
 				break;
 			default: 
-				printf("wrong choice, try again\n");
+				printf("Wrong choice, try again\n");
 		}
 	}
-	//strcpy(message,"Exit");
-	
 		
 	return 0;
 }
